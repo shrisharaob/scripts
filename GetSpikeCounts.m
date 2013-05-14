e758eac1bacfc2bdc1bf6f68e3eff2a3319914ed
Shrisha <shrisha@bach.uni-tuebingen.de>
2013-05-14 21:54:45 +0200

    hmm

--- GetSpikeCounts.m
+++ GetSpikeCounts.m
@@ -1,53 +1,31 @@
 function [sc, winEdges] = GetSpikeCounts(trial, varargin)
-    %  returns spike counts in wins
-    % trial - generic trial class object
-    % winSize in secs
-    % overlap - overlap factor of windows
-    % function sc = GetSpikeCounts(trial, winSize, cluIdx)
+%  returns spike counts in wins
+% trial - generic trial class object
+% winSize in secs
+% statePeriod - in secs
+% overlap - overlap factor of windows
+% function sc = GetSpikeCounts(trial, winSize, cluIdx)
     
-    [winSiz, cluIdx, state, overlap] =  ...
-        DefaultArgs(varargin, {200e-3, [], 'RUN', 0});
-    %   xySegLength = size(trial.position, 1);
-%  winSiz = round(winSiz * trial.sampleRate);
-%  nWins = ceil(xySegLength ./ (trial.trackingSampleRate * winSize));
-%  winEdges = linspace(0, xySegLength / trial.trackingSampleRate, nWins);
+    [winSiz, statePeriods, cluIdx, overlap] =  ...
+        DefaultArgs(varargin, {200e-3, [], [], 0});
+    %  xySegLength = size(trial.position, 1);
+    %  winSiz = round(winSiz * trial.sampleRate);
+    %  nWins = ceil(xySegLength ./ (trial.trackingSampleRate * winSize));
+    %  winEdges = linspace(0, xySegLength / trial.trackingSampleRate, nWins);
     nClus = length(cluIdx);
-    switch trial.datasetType
-      case 'MTA'
-       %  for i = 1:length(trial.states)
-%             if strcmp(state, 'RUN')
-%                 state = 'theta';
-%             end
-%             % if strcmp(trial.states{i}.label, state),
-%                 % statePeriods = trial.states{i}.statePeriods;
-%             break;
-%         end
-%         end
-       
-      case 'kenji'
-        for i = 1:length(trial.states)   
-            if strcmp(trial.states{i}.name, state)
-                statePeriods = trial.states{i}.statePeriods; % @ lfp fs
-                break;
-            end
-        end
-    end
-    if strcmp(trial.datasetType, 'MTA')
-        statePeriods = round(statePeriods .* (trial.sampleRate /  trial.trackingSampleRate))+ 1;
-    else % state periods in lfp sampling rate
-        statePeriods = round(statePeriods .* (trial.sampleRate /  trial.lfpSampleRate))+ 1;  
-    end
     if isempty(trial.res)
-       trial = trial.LoadCR;
+        trial = trial.LoadCR;
     end
-    startEdges = statePeriods(1) : winSiz * (1 - overlap) : statePeriods(2) - winSiz;
-    endEdges = statePeriods(1) + winSiz : winSiz * (1 - overlap) : statePeriods(2);
-    winEdges = [startEdges', endEdges'] / trial.trackingSampleRate;
-    nWins = size(winEdges, 1);
-    sc = zeros(nClus, nWins);
-    [res, origIdx] = SelectPeriods(trial.res, statePeriods, 'd', 1, 1);
-    res = res ./ trial.sampleRate; % res in seconds
+    [res, origIdx] = SelectPeriods(trial.res, statePeriods, 'd');
     clu = trial.clu(origIdx);
+    res = res(ismember(clu, cluIdx));
+    clu = clu(ismember(clu, cluIdx));
+    res = res ./ trial.sampleRate; % res in seconds
+    startEdges = statePeriods(1, 1) ./ trial.sampleRate : winSiz * (1 - overlap) : statePeriods(end, 2) ./ trial.sampleRate - winSiz;
+    endEdges = statePeriods(1, 1) ./ trial.sampleRate + winSiz : winSiz * (1 - overlap) : statePeriods(end, 2) ./ trial.sampleRate;
+    winEdges = [startEdges', endEdges'];
+    nWins = size(winEdges, 1);
+    sc = zeros(nClus, nWins);   
     for mWin = 1 : nWins
         for kClu = 1 : nClus
             kRes  = res(clu == cluIdx(kClu));
